e5adc88b518898a9db8fd6f0186cb9d07ea7abd6
ansbajaj69 <bajajansh822@gmail.com>
2025-10-15 13:05:37 +0530

    user routes and controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -102,7 +102,6 @@ export const logout = async (req, res) => {
     }
 };
 
-
 export const getMe = async(req,res)=>{
     try{
         const user = await User.findById(req.user._id).select("-password");
