925b2aa76e68dcbcc85eab5316a954d85b279f92
ansbajaj69 <bajajansh822@gmail.com>
2025-10-26 08:37:36 +0530

    post routes and controllers added

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,278 @@
+import Notification from "../models/notification.model.js";
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
+import { v2 as cloudinary } from "cloudinary";
+
+export const createPost = async (req, res) => {
+  try {
+    const { text, img } = req.body;
+    const userId = req.user._id.toString();
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    if (!text && !img) {
+      return res .status(400).json({ error: "Post must have text or an image" });
+    }
+
+    let imageUrl = null;
+    if (img) {
+      const uploadedResponse = await cloudinary.uploader.upload(img, {
+        folder: "posts",
+      });
+      imageUrl = uploadedResponse.secure_url;
+    }
+
+    const newPost = new Post({
+      user: userId,
+      text,
+      img: imageUrl,
+    });
+
+    await newPost.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Post created successfully",
+      post: newPost,
+    });
+  } catch (error) {
+    console.error("Error in createPost controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const deletePost = async(req,res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({error: "Post not found"})
+        }
+
+        if(post.user.toString()!==req.user._id.toString()){
+            return res.status(401)({error: "you are not authorized to delete this post"});
+        }
+
+        if(post.img){
+            const imgId = post.img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imgId);
+        }
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({message: "Post deleted successfully"});
+    }
+    catch(error){
+        console.log("Error in deletePost controller",error);
+        res.status(500).json({error: "Internel server error"});
+    }
+};
+
+export const commentOnPost = async (req,res)=>{
+    try{
+        const {text} = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if(!text){
+            return res.status(400).json({ error: "Text field is required" });
+        }
+
+        const post = await Post.findById(postId);
+        if(!post){
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+        const comment = {user: userId , text};
+        post.comments.push(comment);
+        await post.save();
+
+        res.status(200).json(post);
+    }
+    catch(error){
+        console.log("Error in commentOnPost controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const likeUnlikePost = async (req,res)=>{
+    try{
+        const userId = req.user._id;
+		const { id: postId } = req.params;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		const userLikedPost = post.likes.includes(userId);
+        if (userLikedPost) {
+			// Unlike post
+			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+			await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
+
+			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+			res.status(200).json(updatedLikes);
+		} else {
+			// Like post
+			post.likes.push(userId);
+			await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+			await post.save();
+
+			await Notification.create({
+              type: "like",
+              from: userId,
+              to: post.user,
+            });
+			
+			const updatedLikes = post.likes;
+			res.status(200).json(updatedLikes);
+		}
+    }
+    catch(error){
+        console.log("Error in likeUnlikePost controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1; 
+    const limit = parseInt(req.query.limit) || 10; 
+    const skip = (page - 1) * limit; 
+
+    
+    const posts = await Post.find()
+      .sort({ createdAt: -1 }) // newest first
+      .skip(skip) 
+      .limit(limit) 
+      .populate({
+        path: "user",
+        select: "-password -email", 
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password -email",
+      })
+      .lean(); 
+
+ 
+    const totalPosts = await Post.countDocuments();
+
+    if (!posts.length) {
+      return res.status(200).json({
+        posts: [],
+        currentPage: page,
+        totalPages: 0,
+        totalPosts: 0,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      currentPage: page,
+      totalPages: Math.ceil(totalPosts / limit),
+      totalPosts,
+      posts,
+    });
+  } catch (error) {
+    console.error("Error in getAllPosts controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+export const getLikedPosts = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const likedPosts = await Post.find({ _id: { $in: user.likedPosts } })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    res.status(200).json(likedPosts);
+  } catch (error) {
+    console.log("Error in getLikedPosts controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+export const getFollowingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const following = user.following;
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const feedPosts = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    res.status(200).json(feedPosts);
+  } catch (error) {
+    console.log("Error in getFollowingPosts controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error in getUserPosts controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
+
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -23,7 +25,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes)
+app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is runnning on port ${PORT}`);
